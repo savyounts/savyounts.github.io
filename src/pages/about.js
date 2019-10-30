4700e821bbdb10df54d71ab672c9cc4f6907ac54
Savannah Younts <sav@digitalbureau.com>
2019-10-30 16:49:40 -0600

    update about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,17 +7,13 @@ export default() => (
     <Layout>
         <h1 style={{marginLeft: '21%'}}>They call me Sav... </h1>
           <Container>
-            <div style={{paddingRight:'1rem'}}>
-              <p>
+              <p style={{paddingRight:'1rem'}}>
                 My name is Savannah Younts, I am a Full Stack Web Developer based in Denver, CO. I graduated from Clemson University in 2015 with a bachelor's degree in Graphic Communications and graduated from Flatiron School's Full Stack Web Developer program in 2018.
+                <br></br><br></br>
+                Creating is what I was made to do, it's what brings me the most joy. After spending the first several years of my career in the design field and seeing the overlap it has with development, I decided that was the path I wanted focus my career. Currently, am a Web Developer at Digital Bureau where I specialize in React Native and WordPress development.
+                <br></br><br></br>
+                When not online, you'll find me at a park, in the mountains, pretending to be good at woodworking, or possibly on a plane to some new destination waiting to be explored. 
               </p>
-              <p>
-                I have a dream of traveling the world and make a point to do it as much as I can. That being said, I am not afraid to move to new places and meet new people. I have many different hobbies, such as dancing, painting, and athletics and I love to try new things. I believe that trying and learning new things has helped make me a versatile worker and quick learner. Every day is an opportunity to be better than I was yesterday.
-              </p>
-              <p>
-                Living a healthy lifestyle is very important to me, so I stay active by playing several sports at varying levels as well as incorporate other workouts during the off seasons. My love for being active and love of the outdoors that has fueled my passion to design within the sports/outdoors industry. I do my best to stand out and be a little different from everyone around me; to me, normal is boring, so we should each embrace who we really are. With that, I plan to keep doing what I'm doing; learning new skills, exploring new places and always looking for something new to make me better and keep life interesting.
-              </p>
-            </div>
             <img src={me} alt='Savannah' style={{width:'50% !important', marginTop:'0', padding:'0 1rem'}}/>
           </Container>
     </Layout>
